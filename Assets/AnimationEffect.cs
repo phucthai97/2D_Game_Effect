845864bc7d223978bbc71f5edc515275a68e7a59
MSI\hongp <bitpst2015@gmail.com>
2024-04-14 21:09:47 +0700

    +Add button UI

--- Assets/AnimationEffect.cs
+++ Assets/AnimationEffect.cs
@@ -7,12 +7,18 @@ public class AnimationEffect : MonoBehaviour
 {
     [Header(" Settings")]
     [SerializeField] private AnimaEffectType _type;
-    [Space(8)]
 
     [SerializeField] private float _duration = 3f;
+
     [SerializeField] private bool _reverseRotation = false;
     [SerializeField] private float _scaleFactor = 1.2f;
-    [SerializeField] private AnimationCurve _aniCurveScaleInOut;
+
+    /// <summary>
+    /// Setting for sca
+    /// </summary>
+    [SerializeField] private float _scaleFactorX = 1.5f;
+    [SerializeField] private float _scaleFactorY = 1.2f;
+    [SerializeField] public AnimationCurve _aniCurveStrectch;
 
     public enum AnimaEffectType
     {
@@ -21,31 +27,38 @@ public class AnimationEffect : MonoBehaviour
         StetchScale,
     }
 
-
     public AnimaEffectType Type { get => _type; set => _type = value; }
 
     // Start is called before the first frame update
     void Start()
     {
-        Transform targetTransform = transform;
+        Transform targetTrans = transform;
         if (_type == AnimaEffectType.Rotationz)
         {
             float zAxis = -360;
             if (_reverseRotation)
                 zAxis *= -1;
-            targetTransform.DORotate(new Vector3(0f, 0f, zAxis), _duration, RotateMode.FastBeyond360)
+            targetTrans.DORotate(new Vector3(0f, 0f, zAxis), _duration, RotateMode.FastBeyond360)
                                     .SetEase(Ease.Linear)
                                     .SetLoops(-1, LoopType.Restart);
         }
         else if (_type == AnimaEffectType.ScaleInOut)
         {
-            float initialScale = targetTransform.localScale.x;
-            targetTransform.DOScale(initialScale * _scaleFactor, _duration)
+            float initialScale = targetTrans.localScale.x;
+            targetTrans.DOScale(initialScale * _scaleFactor, _duration)
                             .SetEase(Ease.OutSine)
                             .SetLoops(-1, LoopType.Yoyo);
         }
         else if (_type == AnimaEffectType.StetchScale)
         {
+            float initScaleX = targetTrans.localScale.x;
+            float initScaleY = targetTrans.localScale.y;
+            Sequence seq = DOTween.Sequence();
+            seq.Append(targetTrans .DOScale(new Vector3(initScaleX * _scaleFactorX
+                                                    , initScaleY * _scaleFactorY, 1)
+                                                    , _duration)
+                                .SetEase(_aniCurveStrectch))
+                .AppendInterval(1).SetLoops(-1);
 
         }
     }
--- Assets/Editor/AnimationEffectEditor.cs
+++ Assets/Editor/AnimationEffectEditor.cs
@@ -5,10 +5,16 @@ using UnityEditor;
 public class AnimationEffectEditor : Editor
 {
     private bool _showHelp = false;
-
+    
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
+
+        MonoScript scriptPath = MonoScript.FromMonoBehaviour((AnimationEffect)target);
+
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.ObjectField("Script", scriptPath, typeof(MonoScript), false);
+        EditorGUI.EndDisabledGroup();
 
         // Bắt đầu một hàng ngang mới
         GUILayout.BeginHorizontal();
@@ -26,14 +32,17 @@ public class AnimationEffectEditor : Editor
             EditorGUILayout.HelpBox("Follow these steps to make sure everything runs fine.\n1. Download & import Dotween.\n2. Add this element to the object you want to animate. \n3. Choose Effect Type", MessageType.Info);
         }
         EditorGUILayout.Space(4);
-
+        
         AnimationEffect script = (AnimationEffect)target;
 
         // Enum popup
-
         EditorGUILayout.Space();
         EditorGUILayout.LabelField("Animation Settings", EditorStyles.boldLabel);
         script.Type = (AnimationEffect.AnimaEffectType)EditorGUILayout.EnumPopup("Effect Type", script.Type);
+
+        EditorGUILayout.Space(2);
+        DrawSeparator(Color.gray);
+        EditorGUILayout.Space(2);
 
         // Show fields based on selected EffectType
         switch (script.Type)
@@ -49,9 +58,19 @@ public class AnimationEffectEditor : Editor
                 break;
             case AnimationEffect.AnimaEffectType.StetchScale:
                 EditorGUILayout.PropertyField(serializedObject.FindProperty("_duration"), new GUIContent("Duration"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_scaleFactorX"), new GUIContent("ScaleFactorX"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_scaleFactorY"), new GUIContent("ScaleFactorY"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_aniCurveStrectch"), new GUIContent("AniCurveStrectch"));
                 break;
         }
-
         serializedObject.ApplyModifiedProperties();
+    }
+
+    // Hàm vẽ đường gạch ngang với màu tùy chỉnh
+    private void DrawSeparator(Color color)
+    {
+        Rect rect = EditorGUILayout.GetControlRect(false, 1);
+        rect.height = 1.2f;
+        EditorGUI.DrawRect(rect, color);
     }
 }
